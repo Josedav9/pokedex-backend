a78c32da560043d05d8210088de43fb2db614a92
[Gabriel GC] <[gaabgonca@gmail.com]>
2021-09-06 16:45:18 -0500

    Added routes for pokemon-list, legendaries, mythicals and altered all routes to return data from pokemon+species

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -2,7 +2,7 @@ const asyncHandler = require('../middleware/async')
 const Pokedex = require('pokedex-promise-v2')
 const ErrorResponse = require('../utils/errorResponse')
 const axios = require('axios')
-const { raw } = require('express')
+const { raw, request } = require('express')
 
 const P = new Pokedex()
 
@@ -14,6 +14,187 @@ const BASE_URL = 'https://pokeapi.co/api/v2'
 module.exports.pokemon_0 = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, text: 'Hello world' })
 })
+
+// @desc To get list of pokemon with pagination & limits
+// @route GET /api/v1/pokemon/
+// @access public
+module.exports.getPokemons = asyncHandler(async (req, res, next) => {
+  const limit = req.query.limit
+  const offset = req.query.offset
+
+  const pokemonListResponse = await getPokemonListResponse(
+    limit,
+    offset
+  )
+
+  const pokemonList = pokemonListResponse.data.results
+  console.log(pokemonList.length)
+
+  const total = pokemonList.length
+
+  //Get all pokemon within boundaries
+  const pokemonResponses = await axios.all(
+    pokemonList.map(async (pokemon) => await axios.get(pokemon.url))
+  )
+
+  //Get all species within boundaries
+  const speciesResponses = await axios.all(
+    pokemonList.map(
+      async (pokemon) =>
+        await axios.get(
+          pokemon.url.replace('pokemon', 'pokemon-species')
+        )
+    )
+  )
+
+  //Purify pokemon data
+  const pokemonDetails = pokemonResponses.map((response) => {
+    return removeUnnecesaryFieldsFromPokemon(response)
+  })
+
+  //Purify species data
+  const speciesDetails = speciesResponses.map((response) => {
+    return removeUnnecesaryFieldsFromSpecies(response)
+  })
+
+  //Mix pokemon and species
+  const pokemonsAndSpecies = mixPokemonsAndSpecies(
+    pokemonDetails,
+    speciesDetails
+  )
+
+  //Respond
+  res.status(200).json({
+    success: true,
+    data: {
+      count: pokemonsAndSpecies.length,
+      pokemon: pokemonsAndSpecies,
+    },
+  })
+})
+
+// @desc To get list of legendary pokemons with limit and offset
+// @route GET /api/v1/pokemon/legendary/
+// @access public
+module.exports.getLegendaryPokemons = asyncHandler(
+  async (req, res, next) => {
+    const limit = req.query.limit
+    const offset = req.query.offset
+
+    const pokemonListResponse = await getPokemonListResponse(
+      limit,
+      offset
+    )
+
+    const pokemonList = pokemonListResponse.data.results
+    console.log(pokemonList.length)
+
+    //Get all species within boundaries
+    const speciesResponses = await axios.all(
+      pokemonList.map(
+        async (pokemon) =>
+          await axios.get(
+            pokemon.url.replace('pokemon', 'pokemon-species')
+          )
+      )
+    )
+
+    //Filter legendaries
+    const speciesDetails = speciesResponses
+      .map((response) => {
+        return removeUnnecesaryFieldsFromSpecies(response)
+      })
+      .filter((pokemon) => pokemon.is_legendary)
+
+    if (speciesDetails.length == 0)
+      return next(
+        new ErrorResponse(
+          `No legendaries found within the limit and offset stablished`,
+          404
+        )
+      )
+
+    const pokemonDetails = await getPokemonFromSpeciesDetails(
+      speciesDetails
+    )
+
+    //Mix pokemon and species
+    const pokemonsAndSpecies = mixPokemonsAndSpecies(
+      pokemonDetails,
+      speciesDetails
+    )
+
+    //Respond
+    res.status(200).json({
+      success: true,
+      data: {
+        count: pokemonsAndSpecies.length,
+        pokemon: pokemonsAndSpecies,
+      },
+    })
+  }
+)
+
+// @desc To get list of mythical pokemons with limit and offset
+// @route GET /api/v1/pokemon/mythical/
+// @access public
+module.exports.getMythicalPokemons = asyncHandler(
+  async (req, res, next) => {
+    const limit = req.query.limit
+    const offset = req.query.offset
+
+    const pokemonListResponse = await getPokemonListResponse(
+      limit,
+      offset
+    )
+
+    const pokemonList = pokemonListResponse.data.results
+
+    //Get all species within boundaries
+    const speciesResponses = await axios.all(
+      pokemonList.map(
+        async (pokemon) =>
+          await axios.get(
+            pokemon.url.replace('pokemon', 'pokemon-species')
+          )
+      )
+    )
+
+    //Filter mythical
+    const speciesDetails = speciesResponses
+      .map((response) => {
+        return removeUnnecesaryFieldsFromSpecies(response)
+      })
+      .filter((pokemon) => pokemon.is_mythical)
+
+    if (speciesDetails.length == 0)
+      return next(
+        new ErrorResponse(
+          `No mythical found within the limit and offset stablished`,
+          404
+        )
+      )
+
+    const pokemonDetails = await getPokemonFromSpeciesDetails(
+      speciesDetails
+    )
+
+    //Mix pokemon and species
+    const pokemonsAndSpecies = mixPokemonsAndSpecies(
+      pokemonDetails,
+      speciesDetails
+    )
+
+    //Respond
+    res.status(200).json({
+      success: true,
+      data: {
+        count: pokemonsAndSpecies.length,
+        pokemon: pokemonsAndSpecies,
+      },
+    })
+  }
+)
 
 // @desc To get pokemons in a evolutionchain
 // @route GET /api/v1/pokemon/chain/:pokemonId
@@ -28,16 +209,26 @@ module.exports.getEvolutionChain = asyncHandler(
         axios.get(evolutionChainUrl).then(async (response) => {
           const evolutionChain = response.data.chain
           let evolutions = getEvolutionsFromChain(evolutionChain, [])
-          const responses = await axios.all(
+          const speciesDetails = await axios.all(
             evolutions.map((evolution) => axios.get(evolution.url))
           )
-          const evolutionsDetails = responses.map(
-            (response) => response.data
+          const evolutionsDetails = speciesDetails.map((response) =>
+            removeUnnecesaryFieldsFromSpecies(response)
+          )
+
+          const pokemonDetails = await getPokemonFromSpeciesDetails(
+            evolutionsDetails
+          )
+
+          //Mix pokemon and species
+          const pokemonsAndSpecies = mixPokemonsAndSpecies(
+            pokemonDetails,
+            evolutionsDetails
           )
           res.status(200).json({
             success: true,
             evolutions,
-            species: evolutionsDetails,
+            pokemon: pokemonsAndSpecies,
           })
         })
       })
@@ -58,70 +249,58 @@ module.exports.getEvolutionChain = asyncHandler(
 module.exports.getPokemonsFromType = asyncHandler(
   async (req, res, next) => {
     const typeId = req.params.typeId
-    // Pagination
-    const page = parseInt(req.query.page, 10) || 1
-    const limit = parseInt(req.query.limit, 10) || 25
-    const startIndex = (page - 1) * limit
-    let endIndex = page * limit
-
-    const pokemonsFromTypeResponse = await axios
-      .get(BASE_URL + '/type/' + typeId)
-      .catch(function (error) {
-        return next(
-          new ErrorResponse(`Type not found with id ${typeId}`, 404)
-        )
-      })
+    const pokemonsFromTypeResponse = await getListFromType(typeId)
 
     const pokemonsFromType =
       pokemonsFromTypeResponse.data.pokemon.map((value) => {
         return value.pokemon
       })
 
-    console.log('pokemonsFromType', pokemonsFromType)
-
     if (!pokemonsFromType) {
       return next(
         new ErrorResponse(`Type not found with id ${typeId}`, 404)
       )
     } else {
-      if (endIndex > pokemonsFromType.length)
-        endIndex = pokemonsFromType.length
+      // Pagination
+      const total = pokemonsFromType.length
 
-      // Pagination result
-      const pagination = {}
+      const pagination = createPaginationObject(req, total)
 
-      if (endIndex < pokemonsFromType.length) {
-        pagination.next = {
-          page: page + 1,
-          limit,
-        }
-      }
+      //Limit
+      const pokemon = pokemonsFromType.slice(
+        pagination.startIndex,
+        pagination.endIndex
+      )
 
-      if (startIndex > 0) {
-        pagination.prev = {
-          page: page - 1,
-          limit,
-        }
-      }
-      const pokemon = pokemonsFromType.slice(startIndex, endIndex)
-      const responses = await axios.all(
+      //Get all pokemon within boundaries
+      const pokemonResponses = await axios.all(
         pokemon.map(async (pokemon) => await axios.get(pokemon.url))
       )
 
-      const pokemonDetails = responses.map((response) => {
-        const raw_data = response.data
-        delete raw_data.game_indices
-        delete raw_data.held_items
-        delete raw_data.past_types
-        return raw_data
+      //Purify data
+      const pokemonDetails = pokemonResponses.map((response) => {
+        return removeUnnecesaryFieldsFromPokemon(response)
       })
 
+      //Get species details
+      const speciesDetails = await getSpeciesFromPokemonDetails(
+        pokemonDetails
+      )
+
+      //Mix pokemon details and species details
+
+      const pokemonsAndSpecies = mixPokemonsAndSpecies(
+        pokemonDetails,
+        speciesDetails
+      )
+
+      //Respond
       res.status(200).json({
         success: true,
         pagination,
         data: {
-          count: pokemon.length,
-          pokemon: pokemonDetails,
+          count: pokemonsAndSpecies.length,
+          pokemon: pokemonsAndSpecies,
         },
       })
     }
@@ -135,4 +314,136 @@ const getEvolutionsFromChain = (chainRoot, evolutionsList) => {
     return evolutionsList
   }
   return getEvolutionsFromChain(subChain[0], evolutionsList)
+}
+
+const removeUnnecesaryFieldsFromPokemon = (response) => {
+  const raw_data = response.data
+  delete raw_data.game_indices
+  delete raw_data.moves
+  delete raw_data.held_items
+  delete raw_data.past_types
+  delete raw_data.base_experience
+  delete raw_data.stats
+  return raw_data
+}
+
+const removeUnnecesaryFieldsFromSpecies = (response) => {
+  const raw_data = response.data
+  delete raw_data.base_happiness
+  delete raw_data.capture_rate
+  delete raw_data.flavor_text_entries
+  delete raw_data.form_descriptions
+  delete raw_data.genera
+  delete raw_data.names
+  delete raw_data.pal_park_encounters
+  delete raw_data.pokedex_numbers
+  delete raw_data.varieties
+
+  return raw_data
+}
+
+const createPaginationObject = (req, total) => {
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 25
+  const startIndex = (page - 1) * limit
+  let endIndex = page * limit
+  if (endIndex > total) endIndex = total
+
+  // Pagination result
+  const pagination = { startIndex, endIndex }
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    }
+  }
+  return pagination
+}
+
+const getListFromType = async (typeId) => {
+  return axios
+    .get(BASE_URL + '/type/' + typeId)
+    .catch(function (error) {
+      return next(
+        new ErrorResponse(`Type not found with id ${typeId}`, 404)
+      )
+    })
+}
+
+const getPokemonListResponse = async (limit, offset) => {
+  limit ||= 20
+  offset ||= 0
+  console.log('offset', offset)
+  return axios
+    .get(BASE_URL + '/pokemon/', {
+      params: {
+        limit,
+        offset,
+      },
+    })
+    .catch(function (error) {
+      return next(new ErrorResponse(`Unexpected error`, 400))
+    })
+}
+
+const getPokemonResponse = async (pokemonIdOrName) => {
+  return axios
+    .get(BASE_URL + '/pokemon/' + pokemonIdOrName)
+    .catch(function (error) {
+      return next(new ErrorResponse(`Unexpected error`, 400))
+    })
+}
+
+const getSpeciesResponse = async (pokemonIdOrName) => {
+  return axios
+    .get(BASE_URL + '/pokemon-species/' + pokemonIdOrName)
+    .catch(function (error) {
+      return next(new ErrorResponse(`Unexpected error`, 400))
+    })
+}
+
+const getPokemonFromSpeciesDetails = async (speciesDetails) => {
+  //Get pokemon details
+  const pokemonResponses = await axios.all(
+    speciesDetails.map((species) => {
+      return getPokemonResponse(species.name)
+    })
+  )
+
+  //Purify pokemon data
+  const pokemonDetails = pokemonResponses.map((response) => {
+    return removeUnnecesaryFieldsFromPokemon(response)
+  })
+
+  return pokemonDetails
+}
+
+const getSpeciesFromPokemonDetails = async (pokemonDetails) => {
+  //Get pokemon details
+  const speciesResponse = await axios.all(
+    pokemonDetails.map((pokemon) => {
+      return getSpeciesResponse(pokemon.name)
+    })
+  )
+
+  //Purify pokemon data
+  const speciesDetails = speciesResponse.map((response) => {
+    return removeUnnecesaryFieldsFromSpecies(response)
+  })
+
+  return speciesDetails
+}
+
+const mixPokemonsAndSpecies = (pokemonDetails, speciesDetails) => {
+  return pokemonDetails.map((pokemon, index) => {
+    return { ...pokemon, ...speciesDetails[index] }
+  })
 }
--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const {
-  pokemon_0,
   getEvolutionChain,
   getPokemonsFromType,
+  getPokemons,
+  getLegendaryPokemons,
+  getMythicalPokemons,
 } = require('../controllers/pokemons.js')
 
 const router = express.Router()
 
-router.route('/').get(pokemon_0)
+router.route('/').get(getPokemons)
+router.route('/legendary/').get(getLegendaryPokemons)
+router.route('/mythical/').get(getMythicalPokemons)
 router.route('/chain/:pokemonId').get(getEvolutionChain)
 router.route('/type/:typeId').get(getPokemonsFromType)
 
